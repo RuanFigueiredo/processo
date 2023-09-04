5f527c460c53f16a2492a287db229e326b5ec182
RuanFigueiredo <ruanthiago67@gmail.com>
2023-09-04 15:17:14 -0300

    análise de candidatos

--- src/ProcessoSeletivo.java
+++ src/ProcessoSeletivo.java
@@ -1,5 +1,18 @@
 public class ProcessoSeletivo {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        analisarCandidato(1900.00);
+        analisarCandidato(2200.00);
+        analisarCandidato(2000.00);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000;
+        if(salarioBase > salarioPretendido){
+            System.out.println("LIGAR PARA O CANDIDATO");
+        }else if(salarioBase == salarioPretendido ){
+            System.out.println("LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA");
+        }else {
+            System.out.println("AGUARDANDO RESULTADO DOS DEMAIS CANDIDATOS");
+        }
     }
 }
